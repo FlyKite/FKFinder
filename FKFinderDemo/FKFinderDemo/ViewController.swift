7dbdec8fa9dcca756572892fdb7faeeb9ca8e26a
FlyKite <DogeFlyKite@gmail.com>
2017-07-13 15:46:11 +0800

    Initial commit.
    Create project.

---
+++ FKFinderDemo/FKFinderDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FKFinderDemo
+//
+//  Created by 风筝 on 2017/7/13.
+//  Copyright © 2017年 Doge Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
